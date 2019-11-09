3a39b219b4febb9a615f02f89794044d8f66ae5c
Phil <me@Phils-MacBook-Air.local>
2019-11-08 21:31:50 -0600

    added the meal module

--- resolvers/meal.js
+++ resolvers/meal.js
@@ -4,25 +4,24 @@ export default {
     Query: {
         getMeal: (root, {meal_id}, {models}) => models.Meal.findOne({where: {meal_id} }),
         allMeal: (parent, args, {models}) =>  models.Meal.findAll(),
-        // async allMeal(root) {return sequelize.query(
-        //     "SELECT meal_id, ",
-        //     {raw: true, type: sequelize.QueryTypes.SELECT}
-        // )}
+         async allMealJoin(root) {return sequelize.query(
+            "SELECT meal_id, meal_name, m.meal_category_id, description_, m.protein_type_id, protein_type, meal_description, additional_protein_oz FROM meal m JOIN protein_type pt ON m.protein_type_id = pt.protein_type_id JOIN meal_category mc ON m.meal_category_id = mc.meal_category_id WHERE meal_status_id = 2 ORDER BY meal_id DESC",
+             {raw: true, type: sequelize.QueryTypes.SELECT}
+         )}
+
 
     },
 
     Mutation: {
         createMeal: (parent, {input}, {models}) => models.Meal.create(input),
         async updateMeal(_, {input, meal_id}, {models}) {
-            const {meal_status_id,
-                    meal_category_id,
+            const { meal_category_id,
                     protein_type_id,
                     meal_name,
                     meal_description,
                     additional_protein_oz} = input;
             const meal = await models.Meal.findByPk(meal_id);
             await meal.update({
-                meal_status_id,
                 meal_category_id,
                 protein_type_id,
                 meal_name,
--- schema/meal.js
+++ schema/meal.js
@@ -6,31 +6,33 @@ type Meal {
     protein_type_id: Int!
     meal_name: String!
     meal_description: String
-    additional_protein_oz: Int
+    additional_protein_oz: String
 }
 
 type MealReturn{
     meal_id: Int!
-    meal_status_id: Int!
     meal_category_id: Int!
+    description_: String
     protein_type_id: Int!
+    protein_type: String
     meal_name: String!
     meal_description: String
-    additional_protein_oz: Int
+    additional_protein_oz: String
 }
 
 input MealInput{
-    meal_status_id: Int!
+    meal_status_id: Int
     meal_category_id: Int!
     protein_type_id: Int!
     meal_name: String!
     meal_description: String
-    additional_protein_oz: Int
+    additional_protein_oz: String
 }
 
 type Query {
     getMeal(meal_id: Int!): Meal!
     allMeal: [Meal]!
+    allMealJoin: [MealReturn]!
 }
 
 type Mutation {
